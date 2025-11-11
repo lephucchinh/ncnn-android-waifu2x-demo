3a29b15bfd21355c0694e048cd5e874c1b95ce7f
chinhdev <lpchinh25@gmail.com>
2025-11-11 10:27:07 +0700

    update ai enhance local

--- app/src/main/java/pro/archiemeng/waifu2x/MainActivity.kt
+++ app/src/main/java/pro/archiemeng/waifu2x/MainActivity.kt
@@ -24,6 +24,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
@@ -34,7 +35,7 @@ import java.io.File
 import java.io.FileNotFoundException
 import java.lang.Integer.min
 
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity() {
     private var imageView: ImageView? = null
     private var bitmap: Bitmap? = null
     private var yourSelectedImage: Bitmap? = null
@@ -116,9 +117,9 @@ class MainActivity : Activity() {
         upscaler.init(
             assets,
             useGPU,
-            "models-upconv_7_anime_style_art_rgb",
+            "models-upconv_7_photo",
             2,
-            -1,
+            0,
             false,
             numThreads,
             0,
